0a59b515a0f6508d8ba1d610e1aab0a880612e80
Afsana Akhter Khusbu <hadirifat100@gmail.com>
2023-07-20 03:54:18 +0600

    redesign cover section

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,14 @@
 import Button from "@/components/Button/Button";
 import Cover from "@/components/Cover/Cover";
+import FindFabrics from "@/components/FindFabrics/FindFabrics";
 import React from "react";
+
 
 const Home = () => {
   return (
     <div>
       <Cover></Cover>
+      <FindFabrics></FindFabrics>
     </div>
   );
 };
--- src/components/Cover/Cover.js
+++ src/components/Cover/Cover.js
@@ -7,24 +7,33 @@ const Cover = () => {
   return (
     <div className="con">
       <div className="backgroundImage">
-        <Image className="img" src="/images/smiling-face.jpg" width={1000} height={1000} alt="" />
+        <Image
+          className="img"
+          src="/images/smiling-face.jpg"
+          width={1000}
+          height={1000}
+          alt=""
+        />
         <div className="overlay"></div>
-      </div>
-      <div className="container">
-        <div className="p-light  text-blue-200">GARMEN TEXTILE INDUSTRY</div>
-        <div className="w-[70%] max-w-[95%]">
-        <h1 className="text-white">You Can Never Go Wrong With Our Quality</h1>
+
+        <div className="container content">
+          <div className="p-light  text-blue-200">GARMEN TEXTILE INDUSTRY</div>
+          <div className="w-[70%] max-w-[95%]">
+            <h1 className="text-white">
+              You Can Never Go Wrong With Our Quality
+            </h1>
+          </div>
+
+          <p className="text-white">
+            Integer id ipsum eu massa pretium pulvinar non vitae massa donec ut
+            dignissim. Metus aenean vulputate ex sed sem aliquet dapibus. Mauris
+            imperdiet.
+          </p>
+          <section>
+            <Button>DISCOVER MORE</Button>
+            <div className="video-player"></div>
+          </section>
         </div>
-        
-        <p className="text-white">
-          Integer id ipsum eu massa pretium pulvinar non vitae massa donec ut
-          dignissim. Metus aenean vulputate ex sed sem aliquet dapibus. Mauris
-          imperdiet.
-        </p>
-        <section>
-          <Button>DISCOVER MORE</Button>
-          <div className="video-player"></div>
-        </section>
       </div>
     </div>
   );
