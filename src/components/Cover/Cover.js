86770b3b75f5592f9997954d78818f512e7c5c47
Afsana Akhter Khusbu <hadirifat100@gmail.com>
2023-07-19 14:38:51 +0600

    collect image

---
+++ src/components/Cover/Cover.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Button from "../Button/Button";
+
+const Cover = () => {
+  return (
+    <div>
+      <div>
+        <div className="p-light">GARMEN TEXTILE INDUSTRY</div>
+        <h1>You Can Never Go Wrong With Our Quality</h1>
+        <p>
+          Integer id ipsum eu massa pretium pulvinar non vitae massa donec ut
+          dignissim. Metus aenean vulputate ex sed sem aliquet dapibus. Mauris
+          imperdiet.
+        </p>
+        <section>
+          <Button></Button>
+          <div className="video-player"></div>
+        </section>
+      </div>
+    </div>
+  );
+};
+
+export default Cover;
