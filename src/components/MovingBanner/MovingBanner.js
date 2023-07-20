a050c2b5c847d74f4b2c0c8cbdb1d7ef9010d00e
Afsana Akhter Khusbu <hadirifat100@gmail.com>
2023-07-21 05:01:06 +0600

    go for prayer

--- src/components/MovingBanner/MovingBanner.js
+++ src/components/MovingBanner/MovingBanner.js
@@ -1,9 +1,7 @@
 import React from "react";
 import "./MovingBanner.css";
-// import img from "../../img/corporate.jpg";
-import img from "../../image/corporate.jpg"
+import img from "../../image/corporate.jpg";
 import Button from "../Button/Button";
-import Corporate from "../Corporate/Corporate";
 
 const MovingBanner = () => {
   const properties = {
@@ -11,9 +9,22 @@ const MovingBanner = () => {
   };
   return (
     <div className="w-full">
+
         <div style={properties} className="movingBg w-full">
-          <div className=" banner-show ">
-            <Corporate></Corporate>
+          <div className=" banner-show min-h-[500px] flex flex-wrap flex-col justify-evenly gap-1 w-full items-center md:items-start py-12">
+            
+            <h1 className="text-white w-[70%]">
+              Consult With Us The Suitable Material for Your Project
+            </h1>
+            <p className="text-gray-200 w-[75%]">
+              Nulla in nibh at leo faucibus molestie eget nec velit. Phasellus
+              vel felis vel orci iaculis tempor tristique sagittis urna.
+              Phasellus ac ante in lacus tempor egestas.
+            </p>
+            <div>
+              <Button>CONTACT US</Button>
+            </div>
+            
           </div>
         </div>
     </div>
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,11 +1,70 @@
-import React from 'react';
+import React from "react";
+import img from "../../img/background2.png";
+import "./Projects.css";
+
+const projects=[
+    {
+        id:1,
+        title: "Lorem Ipsum",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque, quas!",
+        img:"/images/cloth.jpg"
+    },
+    {
+        id:2,
+        title: "Lorem Ipsum",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque, quas!",
+        img:"/images/sweater.jpg"
+    },
+    {
+        id:3,
+        title: "Lorem Ipsum",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque, quas!",
+        img:"/images/bit-cloth.jpg"
+    },
+    {
+        id:4,
+        title: "Lorem Ipsum",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque, quas!",
+        img:"/images/vertical-clothe.jpg"
+    },
+    {
+        id:5,
+        title: "Lorem Ipsum",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque, quas!",
+        img:"/images/jeans.jpg"
+    },
+    {
+        id:6,
+        title: "Lorem Ipsum",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque, quas!",
+        img:"/images/leather.jpg"
+    },
+]
 
 const Projects = () => {
-    return (
-        <div className='mt-12'>
-            
+  const properties = {
+    backgroundImage: `url("${img.src}") `,
+  };
+  return (
+    <div className="projects w-full mt-12">
+      <div style={properties} className="projectBg">
+        <div className="width flex flex-col flex-wrap justify-center">
+          <span className="customText text-center">OUR PROJECTS</span>
+          <h1 className="text-center">Some of Our Project</h1>
+   
+            <p className="text-center">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod,
+              quidem. A, vel? Sunt sapiente aspernatur sit quod repellendus
+              dignissimos eos?
+            </p>
+            {/* project gallery  */}
+        <div className="project-gallery">
+
         </div>
-    );
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Projects;
